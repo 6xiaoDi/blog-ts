53c175289d268792aa642e6a4012b913d775197c
6xd <ldtheended@163.com>
2021-02-07 19:27:16 +0800

    a0.21（未知类型）

---
+++ 01/dist/10.未知类型.js
@@ -0,0 +1,17 @@
+// let c: any = 'csdn';
+// let d: number = 1;
+// d.toFixed(1);
+// d = c;
+// NumberObject.toFixed(num)
+// d.toFixed(1);
+// let c: unknown = 'csdn';
+// let d: number = 1;
+// d.toFixed(1);
+// d = c;
+// d.toFixed(1);
+//ok
+var c;
+c.a;
+// error
+var d;
+d.a;
---
+++ 01/src/10.未知类型.ts
@@ -0,0 +1,25 @@
+// let c: any = 'csdn';
+
+// let d: number = 1;
+
+// d.toFixed(1);
+
+// d = c;
+// NumberObject.toFixed(num)
+// d.toFixed(1);
+
+// let c: unknown = 'csdn';
+
+// let d: number = 1;
+
+// d.toFixed(1);
+
+// d = c;
+// d.toFixed(1);
+
+//ok
+let c: any;
+c.a;
+// error
+let d: unknown;
+d.a;
