eca654756e7b3502c79d39c9ef6447c84b36e82d
6xd <ldtheended@163.com>
2021-02-07 17:53:12 +0800

    a0.18（枚举类型—字符串类型枚举）

--- 01/dist/07.枚举类型.js
+++ 01/dist/07.枚举类型.js
@@ -10,4 +10,12 @@ HTTP_CODE.OK;
 // 405
 HTTP_CODE.METHOD_NOT_ALLOWED;
 // error
-HTTP_CODE.OK = 1;
+//   HTTP_CODE.OK = 1;
+var URLS;
+(function (URLS) {
+    URLS["USER_REGISETER"] = "/user/register";
+    URLS["USER_LOGIN"] = "/user/login";
+    // 如果前一个枚举值类型为字符串，则后续枚举项必须手动赋值
+    URLS[URLS["INDEX"] = 1] = "INDEX";
+})(URLS || (URLS = {}));
+URLS.USER_LOGIN;
