20e714007598f09c5fbec126d24310b04f6360b0
6xd <ldtheended@163.com>
2021-02-07 15:01:16 +0800

    a0.06（空和未定义类型—`strictNullChecks` 应用）

--- 01/dist/02. 空和未定义类型.js
+++ 01/dist/02. 空和未定义类型.js
@@ -5,7 +5,13 @@
 // // ok
 // // c = null;
 // c = undefined;
-var a;
-a = null;
-// ok（实际运行是有问题的）
-a.toFixed(1);
+// let a:number;
+// a = null;
+// // ok（实际运行是有问题的）
+// a.toFixed(1);
+var ele = document.querySelector('div');
+// ele.style.display = 'none';
+// 获取元素的方法返回的类型可能会包含 null，所以最好是先进行必要的判断，再进行操作
+if (ele) {
+    ele.style.display = 'none';
+}
--- 01/src/02. 空和未定义类型.ts
+++ 01/src/02. 空和未定义类型.ts
@@ -8,7 +8,14 @@
 // // c = null;
 // c = undefined;
 
-let a:number;
-a = null;
-// ok（实际运行是有问题的）
-a.toFixed(1);
+// let a:number;
+// a = null;
+// // ok（实际运行是有问题的）
+// a.toFixed(1);
+
+let ele = document.querySelector('div');
+// ele.style.display = 'none';
+// 获取元素的方法返回的类型可能会包含 null，所以最好是先进行必要的判断，再进行操作
+if (ele) {
+    ele.style.display = 'none';
+}
