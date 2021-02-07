5c7aa22edd60d5949983011fbd9d0b5c5fd45cbc
6xd <ldtheended@163.com>
2021-02-07 14:55:51 +0800

    a0.05（空和未定义类型—隐藏细节问题）

--- 01/dist/02. 空和未定义类型.js
+++ 01/dist/02. 空和未定义类型.js
@@ -1,7 +1,11 @@
+// let a: null;
+// let b: undefined;
+// // a = 1;
+// let c: number;
+// // ok
+// // c = null;
+// c = undefined;
 var a;
-var b;
-// a = 1;
-var c;
-// ok
-// c = null;
-c = undefined;
+a = null;
+// ok（实际运行是有问题的）
+a.toFixed(1);
--- 01/src/02. 空和未定义类型.ts
+++ 01/src/02. 空和未定义类型.ts
@@ -1,9 +1,14 @@
-let a: null;
-let b: undefined;
+// let a: null;
+// let b: undefined;
 
-// a = 1;
+// // a = 1;
 
-let c: number;
-// ok
-// c = null;
-c = undefined;
+// let c: number;
+// // ok
+// // c = null;
+// c = undefined;
+
+let a:number;
+a = null;
+// ok（实际运行是有问题的）
+a.toFixed(1);
