7842c4253b123e99895c214de479d32be4ad2221
6xd <ldtheended@163.com>
2021-02-08 09:38:30 +0800

    a0.26（接口——任意属性——字符串索引）

--- 02/src/1.接口.ts
+++ 02/src/1.接口.ts
@@ -4,16 +4,27 @@
 //     color?: string;
 // }
 
+// interface Point {
+//     readonly x: number;
+//     readonly y: number;
+// }
+
 interface Point {
-    readonly x: number;
-    readonly y: number;
+    x: number;
+    y: number;
+    // color?: string;
+    color?: number;
+    // [key:string]: number;
+    // [key: string]: string  |  undefined;
+    [key: string]: number  |  undefined;
 }
 
 let p1: Point = {
     x: 100,
     y: 100
 };
+p1.z = 500;
 
-p1.x = 200;
+// p1.x = 200;
 
 // let p2 = Point;	//错误
