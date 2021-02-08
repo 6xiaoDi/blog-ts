0b00087d7d008a5adf5eceac1daaab6bc745b475
6xd <ldtheended@163.com>
2021-02-08 14:24:38 +0800

    a0.34（联合类型——使用）

---
+++ 02/dist/2.联合类型.js
@@ -0,0 +1,11 @@
+function css(ele, attr, value) {
+    // ...
+}
+var box1 = document.querySelector('.box');
+// document.querySelector 方法返回值就是一个联合类型
+if (box1) {
+    // ts 会提示有 null 的可能性，加上判断更严谨
+    css(box1, 'width', '100px');
+    css(box1, 'opacity', 1);
+    css(box1, 'opacity', [1, 2]); // 错误
+}
