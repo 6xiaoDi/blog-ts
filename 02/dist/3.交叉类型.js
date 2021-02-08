7395448b1aaea26a6dae090c4f742948bd4faefd
6xd <ldtheended@163.com>
2021-02-08 22:46:14 +0800

    a0.36（注意——api不转换）

--- 02/dist/3.交叉类型.js
+++ 02/dist/3.交叉类型.js
@@ -1,3 +1,13 @@
-;
-;
-var o = Object.assign({}, { x: 1, y: '2' }, { z: 100 });
+// interface o1 {x: number, y: string};
+// interface o2 {z: number};
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
+// let o: o1 & o2 = Object.assign({}, {x:1,y:'2'}, {z: 100});
+var arr = [1, 2, 3];
+var arr2 = __spreadArrays(arr, [10, 20]);
+Promise.resolve();
--- 02/src/3.交叉类型.ts
+++ 02/src/3.交叉类型.ts
@@ -1,4 +1,10 @@
-interface o1 {x: number, y: string};
-interface o2 {z: number};
+// interface o1 {x: number, y: string};
+// interface o2 {z: number};
 
-let o: o1 & o2 = Object.assign({}, {x:1,y:'2'}, {z: 100});
+// let o: o1 & o2 = Object.assign({}, {x:1,y:'2'}, {z: 100});
+
+let arr = [1,2,3];
+
+let arr2 = [...arr, 10, 20];
+
+Promise.resolve();
