1617834dfe39146683655771eadd0b4bc20cff88
6xd <ldtheended@163.com>
2021-02-08 08:59:59 +0800

    a0.25（接口——只读属性）

--- 02/dist/1.接口.js
+++ 02/dist/1.接口.js
@@ -1,5 +1,11 @@
+// interface Point {
+//     x: number;
+//     y: number;
+//     color?: string;
+// }
 var p1 = {
     x: 100,
     y: 100
 };
+p1.x = 200;
 // let p2 = Point;	//错误
--- 02/src/1.接口.ts
+++ 02/src/1.接口.ts
@@ -1,12 +1,19 @@
+// interface Point {
+//     x: number;
+//     y: number;
+//     color?: string;
+// }
+
 interface Point {
-    x: number;
-    y: number;
-    color?: string;
+    readonly x: number;
+    readonly y: number;
 }
 
 let p1: Point = {
     x: 100,
     y: 100
 };
+
+p1.x = 200;
 
 // let p2 = Point;	//错误
