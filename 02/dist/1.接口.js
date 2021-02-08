59b06baa389e5351602edb6cdce483d0b7caca31
6xd <ldtheended@163.com>
2021-02-08 10:48:47 +0800

    a0.31（使用接口描述函数——应用1）

--- 02/dist/1.接口.js
+++ 02/dist/1.接口.js
@@ -3,9 +3,18 @@
 //     y: number;
 //     color?: string;
 // }
-var fn1 = function (a, b) {
+//
+// let fn1: IFunc = function(a: number, b: number): number {
+//     return a + b;
+// }
+// let fn12: IFunc = function(a: number, b: number): number {
+//     return a + b;
+// }
+function todo(callback) {
+    // ....
+    var v = callback(1, 2);
+    // ....
+}
+todo(function (a, b) {
     return a + b;
-};
-var fn12 = function (a, b) {
-    return a + b;
-};
+});
--- 02/src/1.接口.ts
+++ 02/src/1.接口.ts
@@ -53,10 +53,21 @@
 interface IFunc {
     (x: number, y: number): number
 }
+//
+// let fn1: IFunc = function(a: number, b: number): number {
+//     return a + b;
+// }
+// let fn12: IFunc = function(a: number, b: number): number {
+//     return a + b;
+// }
 
-let fn1: IFunc = function(a: number, b: number): number {
-    return a + b;
+function todo(callback: IFunc) {
+    // ....
+    let v = callback(1, 2);
+    // ....
 }
-let fn12: IFunc = function(a: number, b: number): number {
+
+todo( function(a: number, b: number): number {
     return a + b;
-}
+} )
+
