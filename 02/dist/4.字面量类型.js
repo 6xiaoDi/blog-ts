824bcd7a58a170ea7cb01198ad784706f8f6f56e
6xd <ldtheended@163.com>
2021-02-08 23:09:04 +0800

    a0.38（字面量类型）

---
+++ 02/dist/4.字面量类型.js
@@ -0,0 +1,8 @@
+function setPosition(ele, direction) {
+    // ...
+}
+var box2 = document.querySelector('div');
+if (box2) {
+    setPosition(box2, 'left');
+    setPosition(box2, 'a');
+}
---
+++ 02/src/4.字面量类型.ts
@@ -0,0 +1,10 @@
+function setPosition(ele: Element, direction: 'left' | 'top' | 'right' | 'bottom') {
+    // ...
+}
+
+
+let box2 = document.querySelector('div');
+if (box2) {
+    setPosition(box2, 'left');
+    setPosition(box2, 'a');
+}
