0915d568d1b425cb1c6ca70161ada28c14bcf6f3
6xd <ldtheended@163.com>
2021-02-09 13:51:05 +0800

    a0.49（类——成员属性与方法定义）

--- 04/dist/1-类.js
+++ 04/dist/1-类.js
@@ -1,6 +1,16 @@
 var User = /** @class */ (function () {
-    function User() {
+    // 具体特征就是 {} 中进行定义的
+    function User(id, username) {
+        // 创建类的函数，当类通过new实例化的时候，就会执行该函数
+        console.log('这是构造函数，对类中成员属性进行初始化赋值');
+        this.id = id;
+        this.username = username;
     }
+    User.prototype.postArticle = function (title, content) {
+        console.log(this.username + " \u53D1\u8868\u4E86\u4E00\u7BC7\u6587\u7AE0\uFF1A" + title);
+    };
     return User;
 }());
-var user1 = new User();
+var user1 = new User(1, "zhangsan");
+console.log(user1);
+user1.postArticle('标题', '内容');
