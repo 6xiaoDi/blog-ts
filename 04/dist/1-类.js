20b004948ec251338234c91f5babc723c9a8c53b
6xd <ldtheended@163.com>
2021-02-09 14:01:33 +0800

    a0.50（类——构造函数参数属性）

--- 04/dist/1-类.js
+++ 04/dist/1-类.js
@@ -1,8 +1,25 @@
+// class User {
+//     id: number;
+//     username: string;
+//
+//     // 具体特征就是 {} 中进行定义的
+//     constructor(
+//         id: number,
+//         username: string
+//     ) {
+//         // 创建类的函数，当类通过new实例化的时候，就会执行该函数
+//         console.log('这是构造函数，对类中成员属性进行初始化赋值');
+//
+//         this.id = id;
+//         this.username = username;
+//     }
+//
+//     postArticle(title: string, content: string) {
+//         console.log(`${this.username} 发表了一篇文章：${title}`)
+//     }
+// }
 var User = /** @class */ (function () {
-    // 具体特征就是 {} 中进行定义的
     function User(id, username) {
-        // 创建类的函数，当类通过new实例化的时候，就会执行该函数
-        console.log('这是构造函数，对类中成员属性进行初始化赋值');
         this.id = id;
         this.username = username;
     }
