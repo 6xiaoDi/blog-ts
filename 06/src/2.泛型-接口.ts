aeef542537afc373ccc370c3de5459b818c522d9
6xd <ldtheended@163.com>
2021-02-10 15:50:32 +0800

    a0.68（泛型接口）

---
+++ 06/src/2.泛型-接口.ts
@@ -0,0 +1,33 @@
+// 用户接口
+interface IResponseUserData{
+    id: number;
+    username: string;
+    email: string;
+}
+// 文章接口
+interface IResponseArticleData {
+    id: number;
+    title: string;
+    author: IResponseUserData;
+}
+
+interface IResponseData<T>  {
+    code: number;
+    message?: string;
+    data: T;
+}
+
+async function getData<U>(url: string) {
+    let response = await fetch(url)
+    let data:Promise<IResponseData<U>> = await  response.json()
+    return data;
+}
+
+(async  function () {
+    let userData = await getData<IResponseUserData>('/user');
+    userData.data.username;
+
+    let articlesData = await getData<IResponseArticleData>('/article');
+    articlesData.data.author;
+})()
+
