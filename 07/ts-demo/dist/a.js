07405f046a06b846e1ce1b3a431979fe9c456af3
6xd <ldtheended@163.com>
2021-02-10 23:19:32 +0800

    a0.76（ts模块系统——使用02）

--- 07/ts-demo/dist/a.js
+++ 07/ts-demo/dist/a.js
@@ -1,5 +1,17 @@
-import b from './b';
-let a1 = 100;
-let a2 = 200;
-console.log(b);
-export default {};
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports", "./b"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    const b_1 = require("./b");
+    let a1 = 100;
+    let a2 = 200;
+    console.log(b_1.default);
+    exports.default = {};
+});
