0694ebbac872167c7519fafc48b15cd292c2c4e1
6xd <ldtheended@163.com>
2021-02-16 23:14:31 +0800

    a0.84（装饰器——使用1）

---
+++ 08/src/1.装饰器.ts
@@ -0,0 +1,20 @@
+function log(target: Function, name: string, descriptor: PropertyDescriptor) {
+
+    /**
+     * target : 被装饰的方法所属的类
+     * name : 被装饰的方法的名称
+     * descriptor : 描述符
+     */
+    console.log(target, name, descriptor);
+}
+
+class M {
+
+    @log
+    static add(a: number, b: number) {
+        return a + b;
+    }
+}
+
+let v1 = M.add(1, 2);
+console.log(v1);
