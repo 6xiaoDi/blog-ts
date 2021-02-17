5bb76d763ac420aa42c7485cfe1bcc2469a48619
6xd <ldtheended@163.com>
2021-02-17 12:02:39 +0800

    a0.99（装饰器——元数据-reflect-metadata使用2）

--- 08/dist/5.reflect-metadata-demo.js
+++ 08/dist/5.reflect-metadata-demo.js
@@ -1,4 +1,10 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 var A = /** @class */ (function () {
@@ -8,15 +14,18 @@ var A = /** @class */ (function () {
     };
     A.prototype.method2 = function () {
     };
+    __decorate([
+        Reflect.metadata('n', 4)
+    ], A.prototype, "method2", null);
+    __decorate([
+        Reflect.metadata('n', 2)
+    ], A, "method1", null);
+    A = __decorate([
+        Reflect.metadata('n', 1)
+    ], A);
     return A;
 }());
 var obj = new A;
-Reflect.defineMetadata('n', 1, A);
-// 给类的静态方法添加元数据
-Reflect.defineMetadata('n', 2, A, 'method1');
-Reflect.defineMetadata('n', 3, obj);
-// 给对象的实例方法添加元数据
-Reflect.defineMetadata('n', 4, obj, 'method2');
 console.log(Reflect.getMetadata('n', A));
 console.log(Reflect.getMetadata('n', A, 'method1'));
 console.log(Reflect.getMetadata('n', obj));
--- 08/src/5.reflect-metadata-demo.ts
+++ 08/src/5.reflect-metadata-demo.ts
@@ -1,21 +1,17 @@
 import 'reflect-metadata';
 
+@Reflect.metadata('n', 1)
 class A {
+    @Reflect.metadata('n', 2)
     public static method1() {
     }
 
+    @Reflect.metadata('n', 4)
     public method2() {
     }
 }
 
 let obj = new A;
-
-Reflect.defineMetadata( 'n', 1, A );
-// 给类的静态方法添加元数据
-Reflect.defineMetadata( 'n', 2, A, 'method1' );
-Reflect.defineMetadata( 'n', 3, obj );
-// 给对象的实例方法添加元数据
-Reflect.defineMetadata( 'n', 4, obj, 'method2' );
 
 console.log(Reflect.getMetadata('n', A));
 console.log(Reflect.getMetadata('n', A, 'method1'));
